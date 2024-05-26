0e81a2aad53e441b4f83d07ef93a93ae1a20983d
auliakuswina <kuswinaa00@gmail.com>
2024-05-26 20:16:58 +0700

    Initial commit

---
+++ src/main/java/com/example/batch7/ch4/config/Config.java
@@ -0,0 +1,13 @@
+package com.example.batch7.ch4.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Config {
+
+    public  static String APP_NAME ="name";
+
+    public static  String yourName(){
+        return "riki";
+    }
+}
